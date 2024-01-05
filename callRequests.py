7c4e05535918e8ed8070f17e54553cbbb3a40127
Fabian Moor Pucar <fabbem@Fabians-MBP.home>
2024-01-05 08:19:44 +0100

    Fixed bug I think

--- callRequests.py
+++ callRequests.py
@@ -77,12 +77,13 @@ def countCallsForAllUsers():
             response = requests.get("https://api.telavox.se/calls", headers=headers, params=params)
             response.raise_for_status()
             incoming_calls = response.json().get('incoming', [])
-            
-            if previous_calls[username] is None or incoming_calls[0]['callId'] != previous_calls[username]:
-                all_calls[username] += 1
-                print(f"{username} took a call. Added one call.")
-                previous_calls[username] = incoming_calls[0]['callId']  # Update previous_calls
-
+            try:
+                if previous_calls[username] is None or incoming_calls[0]['callId'] != previous_calls[username]:
+                    all_calls[username] += 1
+                    print(f"{username} took a call. Added one call.")
+                    previous_calls[username] = incoming_calls[0]['callId']  # Update previous_calls
+            except IndexError:
+                continue
         except requests.exceptions.RequestException as req_err:
             print(f"Request exception occurred for {username}: {req_err}")
     if today_date != getCurrentDate():
